8921d12064e94b8c1e28cb39113cc63ae336e86d
Jana Koutníková <janca.koutnikova@gmail.com>
2022-10-10 01:07:36 +0200

    Correction after unsuccessful build

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavBar } from "./components/navbar/NavBar";
 import { NotificationContainer } from "./components/notification/NotificationContainer";
 import { AppRoutes } from "./routing/AppRoutes";
-import { StylesProviders } from "./styles/StylesProvider";
+import { StylesProviders } from "./styles/StylesProviders";
 
 export const App = (): JSX.Element => {
 	return (
